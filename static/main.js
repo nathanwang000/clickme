517a290effcb79eaf6289a0388d1dc930815daec
Jiaxuan Wang <nathanwang000@gmail.com>
2015-11-11 00:24:02 -0500

    updated look

--- static/main.js
+++ static/main.js
@@ -1,13 +1,15 @@
 $(window).load(function() {
-    var width = $('#clickBlock').width();
-    $('#clickBlock').css({'height':width+'px'});
+    // var h = $('#clickBlock').height();
+    // $('#clickBlock').css({'width':h+'%'});
+    var w = $('#clickBlock').width();
+    $('#clickBlock').css({'height':w+'px'});
 
     $("#clickBlock").click(function() {
 	// make an ajax call to server to update the clicked count
 	$.ajax({
 	    url: "/click",
 	    success: function(data) {
-		$('#thisHourCount').text("clicked " + data.nclick + " times this hour");
+		$('#thisHourCount').text("clicked " + data.nclick + " times this second");
 	    }
 	})
     });
