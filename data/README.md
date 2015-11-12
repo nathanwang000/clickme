8de99076edef6c1f08da779b756a522763a11d67
Jiaxuan Wang <nathanwang000@gmail.com>
2015-11-11 22:07:15 -0500

    added file

--- data/README.md
+++ data/README.md
@@ -1,5 +1,11 @@
 This folder should contain the data recorded for myself
 
 click.data:
-date_to_hour             number of click
-2015/11/10/10/17         1
+date_to_second           number of click
+2015-11-10-10-17-21      1
+
+tasks/task.data:
+task_name		 From			To
+learn french		 2015-11-10-10-17-21    2015-11-10-10-17-30
+2015-11-10		 1
+2015-11-11		 0
--- run.py
+++ run.py
@@ -1,25 +1,49 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 from collections import defaultdict
 import datetime
 import json
+import re
 
 app = Flask(__name__, template_folder='views', static_folder='static')
 
-now = datetime.datetime.now()
+def getStrTime(time=datetime.datetime.now()):
+    return '-'.join(map(str,[time.year,time.month,time.day,time.hour,
+                             time.minute,time.second]))
+
 # unsafe for multithread, but since only I use it, ok
 thisHourCount = 0 
-lastHour = '-'.join(map(str,[now.year,now.month,now.day,now.hour,now.minute,now.second]))
+lastHour = getStrTime()
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 def index():
-    return render_template('index.html')
+    data = {}
+    if request.method == 'POST':
+        # should trim to alphanumeric, TODO
+        task = re.sub(r"\s"," ",request.form.get('taskdesc').strip())
+        duration = {
+            'week': 7,
+            'day': 1,
+            'month': 31,
+            'year': 365
+        }[request.form.get('duration')]
+        if task:
+            now = datetime.datetime.now()
+            start = getStrTime(now)
+            with open('data/tasks/%s-%s.data' % (task,start),'a') as f:
+                end = getStrTime(now+datetime.timedelta(days=duration))
+                f.write("%s %s %s\n" % (task,start,end))
+        # pass variable to view
+        data['task'] = task
+        data['duration'] = duration
+        print task
+        print 'here'
+    return render_template('index.html',**data)
 
 @app.route('/click')
 def click():
     global thisHourCount, lastHour
     thisHourCount += 1
-    now = datetime.datetime.now()
-    thisHour = '-'.join(map(str,[now.year,now.month,now.day,now.hour,now.minute,now.second]))
+    thisHour = getStrTime()
     # writeback lastHour and update time
     if lastHour != thisHour:
         # write back the data
@@ -29,7 +53,6 @@ def click():
         lastHour = thisHour
     data = {"nclick": thisHourCount}
     return jsonify(**data)
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
